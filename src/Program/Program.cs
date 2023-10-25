d3c3bc3e7c3fa8dfa977e6ca6cebd6521d04c0a9
Martina Laventure <martina.laventure@correo.ucu.edu.uy>
2023-10-25 08:05:32 -0300

    arreglos

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using CompAndDel.Pipes;
 using CompAndDel.Filters;
+using Ucu.Poo.Twitter;
+using Ucu.Poo.Cognitive;
 
 namespace CompAndDel
 {
@@ -8,6 +10,41 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"luke.jpg");
+
+            // Crear filtros
+            IFilter filterNegative = new FilterNegative();
+            IFilter filterGreyscale = new FilterGreyscale();
+            FilterConditional filterConditional = new FilterConditional();
+            IFilter filterTwitter = new FilterTwitter();
+
+            // Crear tuberías
+            IPipe pipeNull = new PipeNull();
+
+            // Crear el PipeConditionalFork para bifurcar según la condición de FilterConditional
+            IPipe hasFacePipe = new PipeSerial(filterTwitter, new PipeNull());
+            IPipe noFacePipe = new PipeSerial(filterNegative, new PipeNull());
+
+            IPipe conditionalForkPipe = new PipeConditionalFork(filterConditional,hasFacePipe, noFacePipe);
+
+            IPipe finalPipe = new PipeSerial(filterGreyscale, conditionalForkPipe);
+
+             // Crear un PictureProvider para la persistencia de imágenes
+            PictureProvider pictureProvider = new PictureProvider();
+
+            // guardar la imagen inicial
+            IPicture initialpicture = picture;
+            pictureProvider.SavePicture(initialpicture, "initialpicture.jpg");
+            
+            // Guardar la imagen intermedia solo si se aplicó el filtro negativo (no hay cara) ya que si hay cara la imagen no vuelve a ser editada.
+            IPicture intermediaResult = noFacePipe.Send(initialpicture);
+            pictureProvider.SavePicture(intermediaResult, "intermediateresult.jpg");
+
+            // Enviar la imagen a través de la tubería final
+            IPicture finalResult = finalPipe.Send(initialpicture);
+            pictureProvider.SavePicture(finalResult, "finalResult.jpg");
+
         }
     }
 }
